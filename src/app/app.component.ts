e4ac95664599effdbe73ab6aaf6ec52f4b71e67d
Mateusz Marek <marek.mateusz@protonmail.com>
2025-05-10 21:27:59 +0200

    Display tasks with completion and remove option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, TodoItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-app';
+  tasks: { text: string; done: boolean }[] = [
+    { text: 'Task 1', done: false },
+    { text: 'Task2', done: true }
+  ];
+
+  markTaskComplete(index: number) {
+    this.tasks[index].done = !this.tasks[index].done;
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
