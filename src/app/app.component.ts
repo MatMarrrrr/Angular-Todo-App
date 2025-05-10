97f306366e888daf5c96d0a51a62ff72813f21d8
Mateusz Marek <marek.mateusz@protonmail.com>
2025-05-10 22:59:41 +0200

    Add todo-form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { CommonModule } from '@angular/common';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, TodoItemComponent],
+  imports: [CommonModule, TodoItemComponent, TodoFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,6 +15,10 @@ export class AppComponent {
     { text: 'Task 1', done: false },
     { text: 'Task2', done: true }
   ];
+
+  addTask(text: string) {
+    this.tasks.push({ text, done: false });
+  }
 
   markTaskComplete(index: number) {
     this.tasks[index].done = !this.tasks[index].done;
---
+++ src/app/components/todo-form/todo-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './todo-form.component.html',
+  styleUrl: './todo-form.component.css'
+})
+export class TodoFormComponent {
+  taskText = '';
+
+  @Output() add = new EventEmitter<string>();
+
+  onSubmit() {
+    const text = this.taskText.trim();
+    if (text) {
+      this.add.emit(text);
+      this.taskText = '';
+    }
+  }
+}
