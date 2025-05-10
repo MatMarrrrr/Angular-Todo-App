0d2ee951a9e2ffb29276981fabda69f771964fef
Mateusz Marek <marek.mateusz@protonmail.com>
2025-05-10 20:56:00 +0200

    Create todo-item component inputs and outputs

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.css'
+})
+export class TodoItemComponent {
+  @Input() text = '';
+  @Input() done = false;
+  @Input() index!: number;
+
+  @Output() remove = new EventEmitter<number>();
+  @Output() markComplete = new EventEmitter<number>();
+
+  onMarkComplete() {
+    this.markComplete.emit(this.index);
+  }
+
+  onRemove() {
+    this.remove.emit(this.index);
+  }
+}
